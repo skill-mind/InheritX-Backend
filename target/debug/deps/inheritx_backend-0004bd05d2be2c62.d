e6b84448c3b256dad4b18c210c5529a8799087f6
Stephanie Nwankwo <stephybelle208@gmail.com>
2025-04-18 13:26:20 +0100

    feat: create project structure

---
+++ target/debug/deps/inheritx_backend-0004bd05d2be2c62.d
@@ -0,0 +1,5 @@
+/Users/user/Downloads/workspace/inheritx-backend/target/debug/deps/libinheritx_backend-0004bd05d2be2c62.rmeta: src/main.rs
+
+/Users/user/Downloads/workspace/inheritx-backend/target/debug/deps/inheritx_backend-0004bd05d2be2c62.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/inheritx_backend-3ae38c9b780630f7.d
@@ -0,0 +1,5 @@
+/Users/user/Downloads/workspace/inheritx-backend/target/debug/deps/libinheritx_backend-3ae38c9b780630f7.rmeta: src/main.rs
+
+/Users/user/Downloads/workspace/inheritx-backend/target/debug/deps/inheritx_backend-3ae38c9b780630f7.d: src/main.rs
+
+src/main.rs:
